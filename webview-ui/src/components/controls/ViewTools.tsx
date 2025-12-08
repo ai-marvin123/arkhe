f3078ad4a726314d6ee045faf80cc375ee3bb544
Mari Lee <marilee.vln@gmail.com>
2025-12-08 13:54:14 -0500

    prepped components and props for viewTools logic

---
+++ webview-ui/src/components/controls/ViewTools.tsx
@@ -0,0 +1,20 @@
+import type { ViewSettings } from '../../state/diagramTypes';
+
+interface ViewToolstype {
+  id: string;
+  view: ViewSettings;
+}
+
+export default function ViewTools({ id, view }: ViewToolstype) {
+  //all features use reducer function 'update_logEntry'
+
+  //logic for pan
+
+  //logic for zoom
+
+  //logic for fullscreen
+
+  //logic for AiMessage
+
+  return <></>;
+}
