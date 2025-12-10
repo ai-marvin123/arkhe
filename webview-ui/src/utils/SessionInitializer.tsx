d600e6fe5f9236618108f5c44aa77d8992c780f8
AvoTheCoder <avothecoder@gmail.com>
2025-12-10 15:03:42 -0800

    Latest working version after merge

--- webview-ui/src/utils/SessionInitializer.tsx
+++ webview-ui/src/utils/SessionInitializer.tsx
@@ -1,6 +1,6 @@
-import { useEffect } from 'react';
-import { useDiagramDispatch } from '../state/diagramContext';
-import { loadSavedDiagram } from '../utils/vsCodeApi';
+import { useEffect } from "react";
+import { useDiagramDispatch } from "../state/diagramContext";
+// import { loadSavedDiagram } from '../utils/vsCodeApi';
 
 export default function SessionInitializer() {
   const dispatch = useDiagramDispatch();
@@ -10,35 +10,35 @@ export default function SessionInitializer() {
     const initializeSession = async () => {
       const newSessionId = crypto.randomUUID();
       dispatch({
-        type: 'initialize_session',
+        type: "initialize_session",
         payload: { sessionId: newSessionId },
       });
 
       //try/catch block to invoke api call
-      try {
-        console.log('inside sessioninitializer try block');
-        const response = await loadSavedDiagram(newSessionId);
+      // try {
+      //   console.log('inside sessioninitializer try block');
+      //   const response = await loadSavedDiagram(newSessionId);
 
-        if (response.command === 'AI_RESPONSE') {
-          const { payload } = response;
+      //   if (response.command === 'AI_RESPONSE') {
+      //     const { payload } = response;
 
-          if (payload.type === 'DIAGRAM') {
-            dispatch({
-              type: 'load_newDiagram',
-              payload: { message: payload.message, data: payload.data },
-            });
-          } else if (payload.type === 'NO_SAVED_DIAGRAM') {
-            dispatch({ type: 'enable_chat' });
-          }
-        } else if (response.command === 'ERROR') {
-          throw new Error(
-            `there was an error checking for saved diagram ${response.payload.message}`
-          );
-        }
-      } catch (error) {
-        console.error('Saved diagram check failed:', error);
-        dispatch({ type: 'enable_chat' });
-      }
+      //     if (payload.type === 'DIAGRAM') {
+      //       dispatch({
+      //         type: 'load_newDiagram',
+      //         payload: { message: payload.message, data: payload.data },
+      //       });
+      //     } else if (payload.type === 'NO_SAVED_DIAGRAM') {
+      dispatch({ type: "enable_chat" });
+      //     }
+      //   } else if (response.command === 'ERROR') {
+      //     throw new Error(
+      //       `there was an error checking for saved diagram ${response.payload.message}`
+      //     );
+      //   }
+      // } catch (error) {
+      //   console.error('Saved diagram check failed:', error);
+      //   dispatch({ type: 'enable_chat' });
+      // }
     };
     initializeSession();
   }, [dispatch]);
