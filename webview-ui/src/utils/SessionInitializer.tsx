d75b3afd25ff2911460837b4b13a3176542dc854
Mari Lee <marilee.vln@gmail.com>
2025-12-19 13:37:12 -0500

    ready for PR

--- webview-ui/src/utils/SessionInitializer.tsx
+++ webview-ui/src/utils/SessionInitializer.tsx
@@ -8,8 +8,7 @@ import {
   MOCK_UNTRACKED_DIAGRAM,
 } from '../../../src/mocks/driftMocks';
 import { startGuidedFlowQ1 } from './guidedFlow';
-
-// import { checkUserApiKey } from '../utils/vsCodeApi';
+import { checkUserApiKey } from '../utils/vsCodeApi';
 /**
  * DEV ONLY:
  * Change this value to test different drift states
@@ -20,42 +19,42 @@ export default function SessionInitializer() {
   const dispatch = useDiagramDispatch();
 
   useEffect(() => {
-    //check if user has OpenAI API key saved
-    // const initializeConnection = async () => {
-    //   try {
-    //     const response = await checkUserApiKey();
-    //     if (!response) {
-    //       throw new Error(
-    //         'No response object returned when inquiring saved user API key'
-    //       );
-    //     }
-    //     if (response.isConfigured === true) {
-    //       return true;
-    //     } else {
-    //       dispatch({
-    //         type: 'load_textOnly',
-    //         payload: {
-    //           message:
-    //             'Please enter your OpenAI API key and model by clicking on the green settings icon above.',
-    //         },
-    //       });
-    //       return false;
-    //     }
-    //   } catch (error) {
-    //     console.error(
-    //       'There was an error initializing connection with user API key',
-    //       error
-    //     );
-    //     return false;
-    //   }
-    // };
+    // check if user has OpenAI API key saved
+    const initializeConnection = async () => {
+      try {
+        const response = await checkUserApiKey();
+        if (!response) {
+          throw new Error(
+            'No response object returned when inquiring saved user API key'
+          );
+        }
+        if (response.isConfigured === true) {
+          return true;
+        } else {
+          dispatch({
+            type: 'load_textOnly',
+            payload: {
+              message:
+                'Please enter your OpenAI API key and model by clicking on the green settings icon above.',
+            },
+          });
+          return false;
+        }
+      } catch (error) {
+        console.error(
+          'There was an error initializing connection with user API key',
+          error
+        );
+        return false;
+      }
+    };
     const executeFlow = async () => {
-      // // Step A: Check API Key Status (MUST AWAIT)
-      // const isConfigured = await initializeConnection();
-      // if (!isConfigured) {
-      //   // Stop execution if key is missing or error occurred
-      //   return;
-      // }
+      // Step A: Check API Key Status (MUST AWAIT)
+      const isConfigured = await initializeConnection();
+      if (!isConfigured) {
+        // Stop execution if key is missing or error occurred
+        return;
+      }
       const newSessionId = crypto.randomUUID();
 
       dispatch({
